0ad648724e6573fa213b45da11ccdcc2dadf31c6
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-07-25 15:18:18 +0300

    basic solve function

--- demo.js
+++ demo.js
@@ -6,3 +6,11 @@ console.log('Mr.' + firstName);
 let name = 'Steve';
 let age = 18;
 let isTrue = false;
+
+//basic solve func
+
+function solve(num1, num2){
+    return num1 + num2;
+}
+
+console.log(solve(9,4));
