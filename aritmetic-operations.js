dd27453fba934c6b47ba0a8038080929875690e0
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-07-25 20:10:00 +0300

    aritmetic operations examples

---
+++ aritmetic-operations.js
@@ -0,0 +1,14 @@
+console.log(5 + 5); //10
+console.log(5 + '5'); //55
+console.log('5' + 5); //55
+console.log(1 + 2 + '3'); //33     
+console.log('1' + 2 + 3); // 123
+
+//division
+console.log(9/2); //4.5
+
+//module division
+console.log(9%2); //1
+
+//exponentiation
+console.log(10 ** 2); //10^2 = 100
