04f10db24aa01f31c6822215a0097815871fac63
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-12 20:45:07 +0300

    3 exercise

--- fundamental-exercises.js
+++ fundamental-exercises.js
@@ -87,3 +87,18 @@
 // }
 
 // getPrice(30, 'Students', 'Sunday');
+
+// function leap(year){
+//     let result;
+
+//     if(year % 4 === 0 && year % 100 !== 0 || year % 400 === 0){
+//         result = 'yes';
+//     }
+//     else {
+//         result = 'no';
+//     }
+
+//     console.log(result);
+// }
+
+// leap(4);
