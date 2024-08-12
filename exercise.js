8f5aa09b5bf7dc13f1e6249b5e239b94ed024a0b
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-12 13:22:22 +0300

    exercise 4 modified

--- exercise.js
+++ exercise.js
@@ -40,11 +40,57 @@
 //     console.log(`The largest number is ${largest}.`);
 // }
 
-function solve(number){
-    if(number >= 5.50){
-        console.log('Excellent')
-    }
-    else{
-        console.log('Not excellent');
+// function solve(number){
+//     if(number >= 5.50){
+//         console.log('Excellent')
+//     }
+//     else{
+//         console.log('Not excellent');
+//     }
+// }
+
+function printMonth(number){
+    switch(number){
+        case 1:
+            console.log('January');
+            break;
+        case 2:
+            console.log('February');
+            break;
+        case 3:
+            console.log('March');
+            break;
+        case 4:
+            console.log('April');
+            break;
+        case 5:
+            console.log('May');
+            break;
+        case 6:
+            console.log('June');
+            break;
+        case 7:
+            console.log('July');
+            break;
+        case 8:
+            console.log('August');
+            break;
+        case 9:
+            console.log('September');
+            break;
+        case 10:
+            console.log('October');
+            break;
+        case 11:
+            console.log('November');
+            break;
+        case 12:
+            console.log('December');
+            break;  
+        default: 
+        console.log('Error!');
+        break;         
     }
 }
+
+printMonth(12);
