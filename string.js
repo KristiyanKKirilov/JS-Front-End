961c20ad74b3f5146da0addad6ec5fbd9a34d31a
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-15 18:19:47 +0300

    text single and double quotes escpaing

---
+++ string.js
@@ -0,0 +1,7 @@
+//escaping
+const quoteText = 'Some"Text"';
+console.log(quoteText);
+const singleQuoteText = "Don't act like a fool";
+console.log(singleQuoteText);
+const escapeSingleQuote = 'Dont\'t mind the single quote\'';
+console.log(escapeSingleQuote);
