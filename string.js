fc6e9417f7adae151c762823bb2d436af04a350e
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-15 18:23:30 +0300

    concatenation options

--- string.js
+++ string.js
@@ -5,3 +5,13 @@ const singleQuoteText = "Don't act like a fool";
 console.log(singleQuoteText);
 const escapeSingleQuote = 'Dont\'t mind the single quote\'';
 console.log(escapeSingleQuote);
+
+//concatenation
+const firstString = 'A';
+const secondString = 'B';
+const ab = firstString + ' - ' + secondString;
+const ab2 = `${firstString} - ${secondString}`;
+const ab3 = firstString.concat(secondString);
+console.log(ab);
+console.log(ab2);
+console.log(ab3);
