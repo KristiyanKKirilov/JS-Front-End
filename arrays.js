e647667211a7c8a58240243eeee0299c53773bbd
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-15 15:24:39 +0300

    splice add and remove method

--- arrays.js
+++ arrays.js
@@ -31,3 +31,22 @@ console.log(namess);
 let newName = namess.shift();
 console.log(namess);
 console.log(newName);
+
+namess.unshift('firstElement');
+console.log(namess);
+let newLength = namess.unshift('more');
+console.log(namess);
+console.log(newLength);
+
+//removing 1 element at first position 
+namess.splice(1, 1);
+console.log(namess);
+
+//Added element to 3 position and moved the current 3th element to 4th position
+namess.splice(3, 0, 'Added the last name');
+console.log(namess);
+
+//removing two elements and assigning them to new array
+let removedNames = namess.splice(0, 2);
+console.log(namess);
+console.log(removedNames);
