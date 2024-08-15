79ccc72710c2c3e4463261555c33161fb77c528c
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-15 14:37:11 +0300

    mutating array methods

--- arrays.js
+++ arrays.js
@@ -6,3 +6,28 @@ const namess = ['Gosho', 'Pesho', 'Sezko'];
 const [firstName, ...restNames] = namess;
 console.log(firstName);
 console.log(restNames);
+
+function solve(numbers){
+    let output ='';
+    for (const number of numbers) {
+        output += `${number} `;
+    }
+
+    console.log(output);
+}
+
+solve([1,2,3,4,5]);
+
+namess.push('New name', 'Another new name', 'Anothe');
+console.log(namess);
+
+namess.pop();
+console.log(namess);
+
+//removing first element
+namess.shift();
+console.log(namess);
+//shift -> can return the element and assign it to variable
+let newName = namess.shift();
+console.log(namess);
+console.log(newName);
