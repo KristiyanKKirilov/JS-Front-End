3cd897abd57dc34ea54b098c55db0bdb98dacb0d
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-16 15:12:29 +0300

    3th exercise / 2 options

--- string-array-lab.js
+++ string-array-lab.js
@@ -17,3 +17,30 @@ function copyAndReverse(count, numbers){
 copyAndReverse(2, arr2);
 
 //3
+function evenOdd(numbers){
+    const even = element => element % 2 == 0;
+    const odd = element => element % 2 != 0;
+    const evenSum = numbers.filter(even);
+    const oddSum = numbers.filter(odd);
+    const evenResult = evenSum.reduce((total, number) => total + number, 0);
+    const oddResult = oddSum.reduce((total, number) => total + number, 0);
+    const result = evenResult - oddResult;
+    console.log(result);
+}
+const array3 = [1,2,3,4,5,6];
+evenOdd(array3);
+
+function secondEvenOdd(numbers){
+    let totalEven = 0;
+    let totalOdd = 0;
+    for (let index = 0; index < numbers.length; index++) {
+        if(numbers[index] % 2== 0){
+            totalEven+= numbers[index];
+        }
+        else{
+            totalOdd += numbers[index];
+        }
+    }
+
+    console.log(totalEven - totalOdd);
+}
