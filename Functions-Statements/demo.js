cf6764aa19942bf3092892a62a64738961417d23
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-17 18:53:01 +0300

    functions and statements demo

---
+++ Functions-Statements/demo.js
@@ -0,0 +1,21 @@
+//function declaration
+function print(text){
+    console.log(text);
+}
+
+//function invocation
+print('text');
+
+//function hoisting
+print2('text2');
+function print2(text){
+    console.log(text);
+}
+
+//function expression
+//anonymous function
+const subtract = function (a,b){
+    console.log(a - b);
+};
+
+subtract(5,4);
