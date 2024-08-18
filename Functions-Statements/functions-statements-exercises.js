3a493c4d5e9b1c8ca4599c8cd5adfd108218f0b3
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-18 21:13:52 +0300

    2nd exercise - used arrow single parameter expression body functions

--- Functions-Statements/functions-statements-exercises.js
+++ Functions-Statements/functions-statements-exercises.js
@@ -1,7 +1,20 @@
 //1
+console.log('-1-');
 function minNum(firstNum, secondNum, thirdNum){
     const numbers = [firstNum, secondNum, thirdNum];
     console.log(Math.min(...numbers));
 }
 
 minNum(9,1,3);
+
+//2
+console.log('-2-')
+function sumAndSubtract(firstNum, secondNum, thirdNum){
+    const sum = (a, b) => a + b;
+    const subtract = (c, d) => c - d;
+    let totalSum = sum(firstNum, secondNum);
+    totalSum = subtract(totalSum, thirdNum);
+    console.log(totalSum);
+}
+
+sumAndSubtract(23, 6, 10);
