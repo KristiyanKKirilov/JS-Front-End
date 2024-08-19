4651a94423ca5c4ae18fc9f22852ad1aebce4fbc
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-20 01:27:52 +0300

    demo object and classes in js

--- Objects-Classes/demo.js
+++ Objects-Classes/demo.js
@@ -1,3 +1,12 @@
 //object literal
-let cat = {name: 'Tom'};
-console.log(cat.name);
+let cat = {name: 'Tom', age: 3};
+console.log(cat);
+
+
+//empty object
+let person = {};
+console.log(person);
+person.name = 'Steve';
+person.age = 20;
+person.isMale = true;
+console.log(person);
