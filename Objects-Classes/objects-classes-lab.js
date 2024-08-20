ea73fb63e20187f3b95af4dfad867b8490295ec2
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-20 13:11:09 +0300

    empty

--- Objects-Classes/objects-classes-lab.js
+++ Objects-Classes/objects-classes-lab.js
@@ -7,7 +7,18 @@ function setPersonalInfo(firstName, lastName, age){
          lastName,
           age,
         };
+
+    // let keys = Object.keys(person);
+    // let values = Object.values(person);
+    let entries = Object.entries(person);
+    for (const keyValuePair of entries) {
+        console.log(`${keyValuePair[0]}: ${keyValuePair[1]}`);     
+    }
+
+    // for (let i = 0; i < keys.length; i++) {
+    //     console.log(`${keys[i]}: ${values[i]}`);        
+    // }
    
 }
 
-setPersonalInfo("Peter","Pan", 20);
+setPersonalInfo("Peter","Pan", "20");
