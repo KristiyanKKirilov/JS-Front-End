081c1fe5441d318fd4445ec2735eda0e57160b55
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-20 12:45:36 +0300

    function expression and arrow function syntax while declaring object

--- Objects-Classes/demo.js
+++ Objects-Classes/demo.js
@@ -10,3 +10,22 @@ person.name = 'Steve';
 person.age = 20;
 person.isMale = true;
 console.log(person);
+console.log(person.name)
+console.log(person['name']);
+
+person['town'] = 'Varna';
+console.log(person.town);
+
+//declare method in literal
+let robot = {
+    name: 'Sony',
+    clean: function(){ //Function expression syntax
+        console.log('Robot cleaning...');
+    },
+    cook: () => console.log('Cooking dinner...'), //Arrow function syntax
+    
+};
+
+console.log(robot);
+robot.clean();
+robot.cook();
---
+++ Objects-Classes/objects-classes-lab.js
@@ -0,0 +1,13 @@
+//1
+console.log('-1-');
+
+function setPersonalInfo(firstName, lastName, age){
+    let person = {
+        firstName,
+         lastName,
+          age,
+        };
+   
+}
+
+setPersonalInfo("Peter","Pan", 20);
