7a0c560741263176524ef84d27f8c890a8ee1739
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-20 13:14:44 +0300

    1st exercise and 2nd exercise

--- Objects-Classes/objects-classes-lab.js
+++ Objects-Classes/objects-classes-lab.js
@@ -8,17 +8,28 @@ function setPersonalInfo(firstName, lastName, age){
           age,
         };
 
-    // let keys = Object.keys(person);
-    // let values = Object.values(person);
-    let entries = Object.entries(person);
-    for (const keyValuePair of entries) {
-        console.log(`${keyValuePair[0]}: ${keyValuePair[1]}`);     
-    }
-
-    // for (let i = 0; i < keys.length; i++) {
-    //     console.log(`${keys[i]}: ${values[i]}`);        
-    // }
-   
+        return person; 
 }
 
 setPersonalInfo("Peter","Pan", "20");
+
+//2
+console.log('-2-');
+
+function printKeyValuePair(object){ 
+    let entries = Object.entries(object);
+    for (const keyValuePair of entries) {
+        console.log(`${keyValuePair[0]} -> ${keyValuePair[1]}`);     
+    }
+
+    
+}
+
+printKeyValuePair({
+    name: "Sofia",
+    area: 492,
+    population: 1238438,
+    country: "Bulgaria",
+    postCode: "1000"
+}
+);
