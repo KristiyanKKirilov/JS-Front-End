60a3bce7642f6e297645759171d2306fdb9ec669
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-22 00:57:52 +0300

    6th lab exercise

--- DOM-Introduction/06.Sum Table/SumTable.js
+++ DOM-Introduction/06.Sum Table/SumTable.js
@@ -1,3 +1,13 @@
 function sumTable() {
+    let allEvenTdElements = document.querySelectorAll('tbody tr td:nth-child(even)');
+    const sumElement = document.getElementById('sum');
+    let totalSum = 0;
+    for (const element of allEvenTdElements) {
+        if(element.textContent){
+            totalSum += Number(element.textContent);
+        }       
+    }
 
+    sumElement.textContent = totalSum;
+   
 }
