229115125ebc9842f5703e21dc10cfbf972353d4
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-22 00:50:30 +0300

    even more optimized solution

--- DOM-Introduction/05.Colorize Table/ColorizeTable.js
+++ DOM-Introduction/05.Colorize Table/ColorizeTable.js
@@ -1,5 +1,5 @@
 function colorize() {
-    let evenRows = document.querySelectorAll('table tr:nth-child(2), table tr:nth-child(4)');
+    let evenRows = document.querySelectorAll('table tr:nth-child(even)');
     console.log(evenRows);
     for (const evenRow of evenRows) {
         evenRow.style.backgroundColor = 'Teal';
