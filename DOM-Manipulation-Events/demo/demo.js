2d1a7394b4e6482c5a8c2e88bc1495638f9d6666
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-22 02:59:00 +0300

    demo continue

--- DOM-Manipulation-Events/demo/demo.js
+++ DOM-Manipulation-Events/demo/demo.js
@@ -10,8 +10,17 @@ function add()
     newMovieElement.textContent = 'The Godfather';
 
     //Append element
-    movieListElement.appendChild(newMovieElement); //add to the end of the collection (as last child)
-    movieListElement.append(newMovieElement); //add to the end of the collection (as last child)
-    movieListElement.prepend(newMovieElement); //add to the beginning of the collection (as first child)
+    //movieListElement.appendChild(newMovieElement); //add to the end of the collection (as last child)
+   // movieListElement.append(newMovieElement); //add to the end of the collection (as last child)
+   // movieListElement.prepend(newMovieElement); //add to the beginning of the collection (as first child)
+
+    //---------------
+    //Clone element and append
+    const firstMovieElement = movieListElement.querySelector('li:first-child');//taking the first element from the list
+    const firstMovieElement2 = movieListElement.children[0];//taking the first element from the list - second option
+    const firstMovieElementClone = firstMovieElement.cloneNode(true);
+    console.log(firstMovieElementClone.textContent);
+    movieListElement.append(firstMovieElementClone);
+
 }
 
