7c94b41e9f8673ea652cdde2355d814b69bd3553
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-22 00:15:28 +0300

    3th exercise - basic calculator

--- DOM-Introduction/03.Sum Numbers/SumNumbers.js
+++ DOM-Introduction/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,6 @@
 function calc() {
-    // TODO: sum = num1 + num2
+   const firstNum = Number(document.getElementById('num1').value);
+   const secondNum = Number(document.getElementById('num2').value);
+   let result = document.getElementById('sum');
+   result.value = firstNum + secondNum;
 }
