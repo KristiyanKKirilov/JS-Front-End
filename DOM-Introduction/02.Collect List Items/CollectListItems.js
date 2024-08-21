a157969b2d9eb2df85455540ca2ad695443fad6f
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-21 19:05:04 +0300

    2nd solution for the second exercise

--- DOM-Introduction/02.Collect List Items/CollectListItems.js
+++ DOM-Introduction/02.Collect List Items/CollectListItems.js
@@ -1,11 +1,11 @@
 function extractText() {
     const ulElement = document.getElementById('items');
     const resultElement = document.getElementById('result');
-    const textResult = ulElement.textContent
-        .split('\n')
-        .map(row => row.trim())
-        .join('\n')
-        .trim();
-    
-        resultElement.textContent = textResult;
+    // const textResult = ulElement.textContent
+    //     .split('\n')
+    //     .map(row => row.trim())
+    //     .join('\n')
+    //     .trim();
+    const textResult = ulElement.innerText;     
+    resultElement.textContent = textResult;
 }
