389858bc3b08a0df07a7e82ed3c31f53f4037031
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-22 01:28:22 +0300

    8th lab exercise

--- DOM-Introduction/07.Extract Parenthesis/ExtractParenthesis.js
+++ DOM-Introduction/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,7 @@
 function extract(content) {
-
+    const regex = /\(([^()]+)\)/g;
+    const contentElement = document.getElementById(content);
+    const matchAllParentheses = contentElement.textContent.matchAll(regex);
+    const result = Array.from(matchAllParentheses).map(match => match[1]).join('; ');
+   return result;
 }
