2d1f56fc2ff587e3559cd7b8c0855dc7734dcad9
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-22 01:10:17 +0300

    even more optimized solution for the sixth lab exericse

--- DOM-Introduction/06.Sum Table/SumTable.js
+++ DOM-Introduction/06.Sum Table/SumTable.js
@@ -2,10 +2,13 @@ function sumTable() {
     let allEvenTdElements = document.querySelectorAll('tbody tr td:nth-child(even):not(#sum');
     const sumElement = document.getElementById('sum');
     let totalSum = 0;
-    for (const element of allEvenTdElements) {
-                 totalSum += Number(element.textContent);             
-    }
+    // for (const element of allEvenTdElements) {
+    //              totalSum += Number(element.textContent);             
+    // }
 
-    sumElement.textContent = totalSum;
+    // sumElement.textContent = totalSum;
+
+    sumElement.textContent = Array.from(allEvenTdElements)
+        .reduce((sum, td) => sum + Number(td.textContent), 0);
    
 }
