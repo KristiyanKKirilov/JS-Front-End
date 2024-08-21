c5583d281d95934879d2387254d852e6690a7927
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-22 00:41:50 +0300

    5th exercise - element by tag name and bc color changed

--- DOM-Introduction/05.Colorize Table/ColorizeTable.js
+++ DOM-Introduction/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,8 @@
 function colorize() {
-    // TODO
+   const rows = document.getElementsByTagName('tr');
+   for (let i = 0; i < rows.length; i++) {
+        if(i % 2 !== 0){
+            rows[i].style.backgroundColor = 'Teal';
+        }    
+   }
 }
