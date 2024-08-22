5f6971d998907dc56e57d64be665fda93f3aa8d7
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-22 05:52:11 +0300

    3th exercise

--- DOM-Manipulation-Events/03. Add-Delete/app.js
+++ DOM-Manipulation-Events/03. Add-Delete/app.js
@@ -1,3 +1,26 @@
 function addItem() {
-    //TODO...
+    //get elements refferences
+    const itemsElement = document.getElementById('items');
+    const newItemTextElement = document.getElementById('newItemText');
+    const inputTextValue = newItemTextElement.value;
+    //create new elementwith textContent of the input
+    const listElement = document.createElement('li');    
+    listElement.textContent = inputTextValue;
+    //create delete button
+    const deleteButton = document.createElement('a');
+    deleteButton.textContent = '[Delete]';
+    //attach event to delete button
+    deleteButton.addEventListener('click', (e) => {
+        // deleteButton.parentElement.remove(); 
+        e.currentTarget.parentElement.remove();
+    })
+
+    //delete button set attribute
+    deleteButton.setAttribute('href', '#');
+    //add button to list element
+    listElement.append(deleteButton);
+    //add list element to DOM
+    itemsElement.appendChild(listElement);
+    //reset input
+    newItemTextElement.value = '';
 }
