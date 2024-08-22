6997a6cb6af3708bc055f8628f3e207c48cddf13
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-22 03:29:35 +0300

    2nd lab exercise

--- DOM-Manipulation-Events/02. Delete-From-Table/app.js
+++ DOM-Manipulation-Events/02. Delete-From-Table/app.js
@@ -1,3 +1,19 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const tableElement = document.getElementById('customers');
+    const inputEmailElement = document.querySelector('input[type="text"][name="email"]');
+    const resultElement = document.getElementById('result');
+
+    const searchEmailValue = inputEmailElement.value;
+
+    const tdElements = tableElement.querySelectorAll('tbody td:last-child');
+    const searchedElement = Array.from(tdElements).find(el => el.textContent === searchEmailValue);
+    if(searchedElement){
+        searchedElement.parentElement.remove();
+        resultElement.textContent = 'Deleted.';
+    }
+    else{
+        resultElement.textContent = 'Not found.';
+    }
+
+    inputEmailElement.value = '';
 }
