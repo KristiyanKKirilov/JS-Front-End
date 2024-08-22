9f90a750afd0620c5401934fa7f00f87a9936afa
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-08-22 07:17:30 +0300

    6th lab exercise

--- DOM-Manipulation-Events/06. Dynamic-Validation/app.js
+++ DOM-Manipulation-Events/06. Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    console.log('TODO:...');
+    const inputEmailElement = document.getElementById('email');
+    const pattern = /[a-z]+@[a-z]+\.[a-z]+/g;
+    inputEmailElement.addEventListener('change', (e) => {
+            if(pattern.test(e.currentTarget.value)){
+                e.currentTarget.classList.remove('error');
+            }   
+            else{
+                e.currentTarget.classList.add('error');
+            }     
+    })
 }
