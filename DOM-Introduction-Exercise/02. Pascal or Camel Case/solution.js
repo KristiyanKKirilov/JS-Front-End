02377a7e00194d1975fd1fb6c5349dd58acda7d2
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-09-07 18:04:30 +0300

    dom exercise 02

--- DOM-Introduction-Exercise/02. Pascal or Camel Case/solution.js
+++ DOM-Introduction-Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,19 @@
 function solve() {
-  //TODO...
+  let input = document.getElementById('text').value;
+  let caseRequired = document.getElementById('naming-convention').value;
+  let result = document.getElementById('result');
+  const validNamingConventions = ['Camel Case', 'Pascal Case'];
+  if(!validNamingConventions.includes(caseRequired)){
+    result.textContent = 'Error!';
+    return;
+  }
+  
+  const pascalCaseText = input.toLowerCase()
+  .split(' ')
+  .map(x => x[0].toUpperCase().concat(x.slice(1)))
+  .join('');
+  
+  result.textContent = caseRequired === validNamingConventions[0]
+  ? pascalCaseText[0].toLowerCase().concat(pascalCaseText.slice(1))
+  : pascalCaseText;
 }
