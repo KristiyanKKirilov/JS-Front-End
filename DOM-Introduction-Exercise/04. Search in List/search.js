0e01f45c124a003dd5a8b2136bb88fae54afcc49
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-09-08 19:21:50 +0300

    dom exercise 04

--- DOM-Introduction-Exercise/04. Search in List/search.js
+++ DOM-Introduction-Exercise/04. Search in List/search.js
@@ -1,3 +1,25 @@
 function search() {
-   // TODO
+   const townEls = document.querySelectorAll('#towns li');
+   const resultEl = document.querySelector('#result');
+   function getMatchElements(input, elements) {
+      return [...elements]
+      .filter(x => x.textContent.toLowerCase().includes(input.toLowerCase()));
+   }
+
+   function clearPreviousMatches(){
+      townEls.forEach(el => {
+         el.style.fontWeight = 'normal';
+         el.style.textDecoration = 'none';
+      })
+   }
+
+   clearPreviousMatches();
+   const [inputEl] = document.getElementsByTagName('input');
+   const matchElements = getMatchElements(inputEl.value, townEls);
+   matchElements.forEach(matchEl => {
+      matchEl.style.fontWeight = 'bold';
+      matchEl.style.textDecoration = 'underline';
+   })
+
+   resultEl.textContent =  `${matchElements.length} matches found`;
 }
