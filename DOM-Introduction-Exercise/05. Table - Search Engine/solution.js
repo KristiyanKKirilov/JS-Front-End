f7d4496cda73b0142768c001fb74b12f67388ec7
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-09-10 14:46:26 +0300

    dom exercise 05

--- DOM-Introduction-Exercise/05. Table - Search Engine/solution.js
+++ DOM-Introduction-Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,23 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const rowEls = document.querySelectorAll('table.container tbody tr');
+      const input = document.querySelector('#searchField').value;
+
+      function getMatchElements(input){
+         return [...rowEls].filter((row) => row.textContent.toLowerCase().includes(input.toLowerCase()));
+      }
+
+      function clearPreviousMatches(){
+         [...rowEls].forEach(el => {
+            el.classList.remove('select');
+         })
+      }
+
+      clearPreviousMatches();
+      const matches = getMatchElements(input);
+
+      matches.forEach(match => match.classList.add('select'));
 
    }
 }
