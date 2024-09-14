a67b21a997497e19b4c5cec8ee7d81e43ad3f64c
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-09-14 22:09:16 +0300

    07

--- DOM-Introduction-Exercise/07. Hell's Kitchen/app.js
+++ DOM-Introduction-Exercise/07. Hell's Kitchen/app.js
@@ -1,8 +1,40 @@
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
+   const inputEl = document.querySelector('#inputs textarea');
+   const bestRestaurantsEl = document.querySelector('#outputs #bestRestaurant p');
+   const workersEl = document.querySelector('#outputs #workers p');
+
    function onClick () {
-      //   TODO:
       
+      const restaurants = JSON.parse(inputEl.value).reduce((acc, data) => {
+         const [restaurantName, workersData] = data.split(' - ');
+         const workers = workersData.split(', ').map((workersData) => {
+            const [name, salary] = workersData.split(' ');
+            return {
+               name,
+               salary: Number(salary)
+            }
+         })
+
+         if(!acc.hasOwnProperty(restaurantName)){
+            acc[restaurantName] = {
+               workers: []
+            }
+         }
+
+         acc[restaurantName].workers.push(...workers);
+         return acc;
+      }, {});
+   
+      function getAvgSalary(restaurantData){
+         return restaurantData.workers.reduce((a,b) => a + b.salary, 0) / restaurantData.workers.length;
+      }
+
+      const [bestRestaurant] = Object.keys(restaurants).sort(
+         (a,b) => getAvgSalary(restaurants[b]) - getAvgSalary(restaurants[a]));
+      
+      const bestWorkers = restaurants[bestRestaurant].workers.slice().sort((a, b) => b - a);
+      bestRestaurantsEl.textContent =`Name: ${bestRestaurant} Average Salary: ${getAvgSalary(restaurants[bestRestaurant]).toFixed(2)} Best Salary: ${bestWorkers[0].salary.toFixed(2)}`
    }
 }
