b4cf4382c9f1734e096946714fd7ffa8f049d42c
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-09-14 13:27:36 +0300

    dom exercise 06

--- DOM-Introduction-Exercise/06. Format the Text/solution.js
+++ DOM-Introduction-Exercise/06. Format the Text/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-  //TODOasas
+  const inputEl = document.getElementById('input');
+  const outputEl = document.getElementById('output');
+  const sentences = inputEl.value.split('. ').map((x) => x.trim().concat('.'));
+
+  function createParagraphEl(text){
+    const paragraphEl = document.createElement('p');
+    paragraphEl.textContent = text;
+    return paragraphEl;
+  }
+
+  for(let i = 0; i < sentences.length; i+=3){
+    const currentText = sentences.slice(i, i + 3).join(' ');
+    const currentParagraph = createParagraphEl(currentText);
+    outputEl.appendChild(currentParagraph);
+  }
+
 } 
