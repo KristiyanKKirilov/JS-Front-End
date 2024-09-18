bc80253ed6d07f0d2acd204b6c024844f61ee581
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-09-18 23:21:18 +0300

    dom exercise 08

--- DOM-Introduction-Exercise/08. Generate Report/report.js
+++ DOM-Introduction-Exercise/08. Generate Report/report.js
@@ -1,3 +1,16 @@
 function generateReport() {
-    //TODO
+    const theadRows = document.querySelectorAll('thead tr th');
+    const tbodyRows = document.querySelectorAll('tbody tr');
+    const outputEl = document.querySelector('#output');
+    const checkedInputEls = [...theadRows]
+    .map((x, i) => ({input: x.children[0], index: i }))
+    .filter(x => x.input.checked);
+
+    const outputData = [...tbodyRows].map((tr) => {
+        return checkedInputEls.reduce((acc, curr) => {
+            acc[curr.input.name] = tr.children[curr.index].textContent;
+            return acc;
+        }, {});
+    });
+    outputEl.value = JSON.stringify(outputData);
 }
