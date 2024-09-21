c5e49b875b3ba9330833281972ae0d0b82efbad5
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-09-21 19:43:44 +0300

    second option for displaying div element

--- DOM-Manipulation-Events-Exercises/01. Sections/script.js
+++ DOM-Manipulation-Events-Exercises/01. Sections/script.js
@@ -17,6 +17,7 @@ function create(words) {
 
       divEl.addEventListener('click', () => {
          divEl.children[0].style.display = 'block';
+         //event.target.children[0].style.display = 'block';
       });
       divContentEl.appendChild(divEl);      
    });
