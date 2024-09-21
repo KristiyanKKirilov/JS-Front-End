7f4ee9288170e0d146843fedab53f9227ecb0fdc
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-09-21 19:42:36 +0300

    dom manipulation and events 01 exercise

--- DOM-Manipulation-Events-Exercises/01. Sections/script.js
+++ DOM-Manipulation-Events-Exercises/01. Sections/script.js
@@ -1,6 +1,23 @@
 function create(words) {
    const divContentEl = document.getElementById('content');
-   words.forEach((word) => {
+
+   function createWordStructute(word){
       const pEl = document.createElement('p');
+      const divEl = document.createElement('div');
+
+      pEl.textContent = word;
+      pEl.style.display = 'none';
+
+      divEl.appendChild(pEl);
+
+      return divEl;
+   }
+   words.forEach((word) => {
+      const divEl = createWordStructute(word);
+
+      divEl.addEventListener('click', () => {
+         divEl.children[0].style.display = 'block';
+      });
+      divContentEl.appendChild(divEl);      
    });
 }
