0ec2f4f65eeb00b5bf087be26cdc4ee1b7602b0d
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-09-22 12:16:33 +0300

    seconds to min, hours, days

--- DOM-Manipulation-Events-Exercises/02. Time-Converter/solution.js
+++ DOM-Manipulation-Events-Exercises/02. Time-Converter/solution.js
@@ -1,4 +1,26 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    const secondsToMinutes = (seconds) => seconds / 60;
+    const secondsToHours = (seconds) => secondsToMinutes(seconds) / 60;
+    const secondsToDays = (seconds) => secondsToHours(seconds) / 24;
+
+    function onClickHandler(event){
+        const rawId = event.target.getAttribute('id');
+        const inputId = rawId.replace('Btn', '');
+
+        const inputEl = document.querySelector(`input#${inputId}`); 
+
+        switch(inputId){
+            case 'seconds': {
+                document.getElementById('days').value = secondsToDays(inputEl.value);
+                document.getElementById('hours').value = secondsToHours(inputEl.value);
+                document.getElementById('minutes').value = secondsToMinutes(inputEl.value);
+            }
+        }
+    }
+
+    document.querySelectorAll('input[type="button"]').forEach((buttonEl) => {
+        buttonEl.addEventListener('click', onClickHandler);
+    });
+
 }
