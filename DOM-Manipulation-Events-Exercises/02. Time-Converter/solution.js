7b2f177de242287f85d80743d7f00f431850a633
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-09-22 12:24:16 +0300

    minutes to sec, hours, days functionality added

--- DOM-Manipulation-Events-Exercises/02. Time-Converter/solution.js
+++ DOM-Manipulation-Events-Exercises/02. Time-Converter/solution.js
@@ -3,6 +3,10 @@ function attachEventsListeners() {
     const secondsToMinutes = (seconds) => seconds / 60;
     const secondsToHours = (seconds) => secondsToMinutes(seconds) / 60;
     const secondsToDays = (seconds) => secondsToHours(seconds) / 24;
+    
+    const minutesToSeconds = (minutes) => minutes * 60;
+    const minutesToHours = (minutes) => minutes / 60;
+    const minutesToDays = (minutes) => minutesToHours(minutes) / 24;
 
     function onClickHandler(event){
         const rawId = event.target.getAttribute('id');
@@ -16,6 +20,13 @@ function attachEventsListeners() {
                 document.getElementById('hours').value = secondsToHours(inputEl.value);
                 document.getElementById('minutes').value = secondsToMinutes(inputEl.value);
             }
+            break;
+            case 'minutes': {
+                document.getElementById('days').value = minutesToDays(inputEl.value);
+                document.getElementById('hours').value = minutesToHours(inputEl.value);
+                document.getElementById('seconds').value = minutesToSeconds (inputEl.value);
+            }
+            break;
         }
     }
 
