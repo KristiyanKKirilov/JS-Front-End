575cce255576f90fd7731bd24d4f28b7a0d4e28e
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-09-23 23:25:28 +0300

    encapsulating code

--- DOM-Manipulation-Events-Exercises/06. Furniture/solution.js
+++ DOM-Manipulation-Events-Exercises/06. Furniture/solution.js
@@ -4,24 +4,27 @@ function solve() {
 
   const tbodyEl = document.querySelector('tbody');
   const firstTrEl = tbodyEl.querySelector('tr');
+
+  function appendTrForEachData({img, name, price, decFactor}) {
+    const currentTrClone = firstTrEl.cloneNode(true);
+    //changing the image 
+    currentTrClone.children[0].children[0].setAttribute('src', img);
+    //changing the name
+    currentTrClone.children[1].children[0].textContent = name;
+    //changing the price 
+    currentTrClone.children[2].children[0].textContent = price;
+    //changing the decFactor
+    currentTrClone.children[3].children[0].textContent = decFactor;
+    //changing the checkbox availability
+    currentTrClone.children[4].children[0].disabled = false;
+    tbodyEl.appendChild(currentTrClone);
+  }
+
   function onGenerateBtnClickHandler(){
     const inputData = JSON.parse(inputTextarea.value);
     console.log(inputData);
 
-    inputData.forEach(({img, name, price, decFactor}) => {
-      const currentTrClone = firstTrEl.cloneNode(true);
-      //changing the image 
-      currentTrClone.children[0].children[0].setAttribute('src', img);
-      //changing the name
-      currentTrClone.children[1].children[0].textContent = name;
-      //changing the price 
-      currentTrClone.children[2].children[0].textContent = price;
-      //changing the decFactor
-      currentTrClone.children[3].children[0].textContent = decFactor;
-      //changing the checkbox availability
-      currentTrClone.children[4].children[0].disabled = false;
-      tbodyEl.appendChild(currentTrClone);
-    })
+    inputData.forEach(appendTrForEachData);
   }
 
   generateBtn.addEventListener('click', onGenerateBtnClickHandler);
