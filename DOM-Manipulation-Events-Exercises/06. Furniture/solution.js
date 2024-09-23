e0854b2c9d9d1587fda6a50198c89841226da731
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-09-23 22:43:41 +0300

    cloning the first tr element in the tbody and changing the image for every object in the array

--- DOM-Manipulation-Events-Exercises/06. Furniture/solution.js
+++ DOM-Manipulation-Events-Exercises/06. Furniture/solution.js
@@ -2,8 +2,16 @@ function solve() {
   const [generateBtn, buyBtn] = document.querySelectorAll('button');
   const [inputTextarea, outputTextarea] = document.querySelectorAll('textarea');
 
+  const firstTrEl = document.querySelector('tbody > tr');
   function onGenerateBtnClickHandler(){
-    console.log(inputTextarea.value);
+    const inputData = JSON.parse(inputTextarea.value);
+    inputData.forEach(({img, name, price, decFactor}) => {
+      const currentTrClone = firstTrEl.cloneNode(true);
+
+      //changing the image 
+      currentTrClone.children[0].setAttribute('src', img);
+    
+    })
   }
 
   generateBtn.addEventListener('click', onGenerateBtnClickHandler);
