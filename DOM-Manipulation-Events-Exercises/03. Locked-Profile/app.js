a7e7ade905db9cf3b37da55b64f390f9f8b0df20
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-09-24 15:34:56 +0300

    locked profile exercise DOM manipulation 03

--- DOM-Manipulation-Events-Exercises/03. Locked-Profile/app.js
+++ DOM-Manipulation-Events-Exercises/03. Locked-Profile/app.js
@@ -1,3 +1,20 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const buttonEls = document.querySelectorAll('div.profile button');
+
+    function onShowMoreBtnClickHandler(event) {
+        const buttonEl = event.target;
+        const unlockInputEl = buttonEl.parentElement.querySelectorAll('input[type="radio"]')[1];
+        if (unlockInputEl.checked) {
+            if (buttonEl.textContent === 'Show more') {
+                buttonEl.parentElement.querySelector('div').style.display = 'block';
+                buttonEl.textContent = 'Hide it';
+            }
+            else {
+                buttonEl.parentElement.querySelector('div').style.display = 'none';
+                buttonEl.textContent = 'Show more';
+            }
+        }      
+
+    }
+    buttonEls.forEach((buttonEl) => buttonEl.addEventListener('click', onShowMoreBtnClickHandler));
 }
