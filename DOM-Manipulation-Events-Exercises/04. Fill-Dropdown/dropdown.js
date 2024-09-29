cdf6e2d62e59a127a4229c0b4dd7b8bc85539f59
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-09-29 16:50:00 +0300

    function creating option element

--- DOM-Manipulation-Events-Exercises/04. Fill-Dropdown/dropdown.js
+++ DOM-Manipulation-Events-Exercises/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,8 @@
 function addItem() {
-    console.log('TODO:...');
+    function createOptionElement(text, value){
+        const option = document.createElement('option');
+        option.textContent = text;
+        option.value = value;
+    }
+    const [textInputEl, valueInputEl] = document.querySelectorAll('input[type="text"]');
 }
