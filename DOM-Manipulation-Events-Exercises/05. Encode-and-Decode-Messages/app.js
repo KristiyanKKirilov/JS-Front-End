89b93efea65262d12f11c560cee693cf97d869ed
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-09-29 17:12:01 +0300

    04 exercise structure created

--- DOM-Manipulation-Events-Exercises/05. Encode-and-Decode-Messages/app.js
+++ DOM-Manipulation-Events-Exercises/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,15 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const [encodeBtnEl, decodeBtnEl] = document.querySelectorAll('div button');
+    const [encodeTextAreaEl, decodeTextAreaEl] = document.querySelectorAll('div textarea');
+
+    function encodeMessageHandler(){
+
+    }
+
+    function decodeMessageHandler(){
+        
+    }
+
+    encodeBtnEl.addEventListener('click', encodeMessageHandler);
+    decodeBtnEl.addEventListener('click', decodeMessageHandler);
 }
