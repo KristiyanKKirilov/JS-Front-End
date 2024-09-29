cc9ed6123398964635f467f6b0a601e07d5596b3
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-09-29 17:23:58 +0300

    encodeMessageHandler implemented

--- DOM-Manipulation-Events-Exercises/05. Encode-and-Decode-Messages/app.js
+++ DOM-Manipulation-Events-Exercises/05. Encode-and-Decode-Messages/app.js
@@ -2,8 +2,17 @@ function encodeAndDecodeMessages() {
     const [encodeBtnEl, decodeBtnEl] = document.querySelectorAll('div button');
     const [encodeTextAreaEl, decodeTextAreaEl] = document.querySelectorAll('div textarea');
 
+    function turnIntoNextChar(char){
+        const charCode = char.charCodeAt(0) + 1;
+        return String.fromCharCode(charCode);
+    }
+    
     function encodeMessageHandler(){
-
+        let encodeTextAreaValue = encodeTextAreaEl.value;        
+        let result = encodeTextAreaValue.split('').map(ch => turnIntoNextChar(ch)).join('');
+        decodeTextAreaEl.value = result;
+        encodeTextAreaEl.value = '';
+        
     }
 
     function decodeMessageHandler(){
