7b100ae63ef22c8bc4494aee89fadd52edc42ade
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-09-29 15:04:01 +0300

    06 dom manipulation exercise

--- DOM-Manipulation-Events-Exercises/06. Furniture/solution.js
+++ DOM-Manipulation-Events-Exercises/06. Furniture/solution.js
@@ -7,17 +7,32 @@ function solve() {
 
   function appendTrForEachData({ img, name, price, decFactor }) {
     const currentTrClone = firstTrEl.cloneNode(true);
-    //changing the image 
-    currentTrClone.children[0].children[0].setAttribute('src', img);
+    //changing the image `
+    const imgTd = currentTrClone.children[0];
+    imgTd.children[0].setAttribute('src', img);
+    imgTd.innerHTML = imgTd.innerHTML.trim();
+
     //changing the name
-    currentTrClone.children[1].children[0].textContent = name;
+    const nameTd = currentTrClone.children[1];
+    nameTd.children[0].textContent = name;
+    nameTd.innerHTML = nameTd.innerHTML.trim();
+
     //changing the price 
-    currentTrClone.children[2].children[0].textContent = price;
+    const priceTd = currentTrClone.children[2];
+    priceTd.children[0].textContent = price;
+    priceTd.innerHTML = priceTd.innerHTML.trim();
+
     //changing the decFactor
-    currentTrClone.children[3].children[0].textContent = decFactor;
+    const decFactorTd = currentTrClone.children[3];
+    decFactorTd.children[0].textContent = decFactor;
+    decFactorTd.innerHTML = decFactorTd.innerHTML.trim();
+
     //changing the checkbox availability
     const checkboxEl = currentTrClone.children[4].children[0];
     checkboxEl.disabled = false;
+    checkboxEl.innerHTML = checkboxEl.innerHTML.trim();
+    currentTrClone.children[4].innerHTML = currentTrClone.chidren[4].innerHTML.trim();
+
     checkboxEl.setAttribute('name', name);
     checkboxEl.setAttribute('price', price);
     checkboxEl.setAttribute('decFactor', decFactor);
@@ -27,18 +42,20 @@ function solve() {
 
   function onGenerateBtnClickHandler() {
     const inputData = JSON.parse(inputTextarea.value);
-    console.log(inputData);
 
     inputData.forEach(appendTrForEachData);
   }
 
   function onBuyBtnClickHandler() {
     const outputData = [...document.querySelectorAll('input[type="checkbox"]')]
-    .filter((inputEl) => inputEl.checked).reduce((acc, currInputEl) => {
-      const name = currInputEl.getAttribute('name');
-      const price = currInputEl.getAttribute('price');
-      const decFactor = currInputEl.getAttribute('decFactor');
+    .filter((inputEl) => inputEl.checked)
+    .reduce((acc, currInputEl) => {
+      const name = currInputEl.closest('tr').getAttribute('name');
+      const price = currInputEl.closest('tr').getAttribute('price');
+      const decFactor = currInputEl.closest('tr').getAttribute('decFactor');
 
+      currInputEl.parentElement.innerHTML = currInputEl.parentElement.innerHTML.trim();
+      
       acc.names.push(name);
       acc.totalPrice += Number(price);
       acc.totalDecFactor += Number(decFactor); 
