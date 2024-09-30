ccc369601391e370b451e60759dc2659cb9d23ec
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-09-30 18:02:53 +0300

    09 exericse - when button clicked, answer is being saved

--- DOM-Manipulation-Events-Exercises/09. JavaScript-Quiz/solution.js
+++ DOM-Manipulation-Events-Exercises/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,12 @@
 function solve() {
-  //TODO...
+  const correctAnswers = ["onclick"]
+  const userAnswers = [];
+
+  function onclickHandler(event){
+    userAnswers.push(event.target.textContent);
+  }
+  document.querySelectorAll('.answer-wrap')
+          .forEach((wrapEl) => wrapEl.addEventListener('click', onclickHandler));
+
+   
 }
