cceb297e6bad3521b3da7452410c2c0381b49e08
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-10-08 21:07:10 +0300

    vendor module installed and demo made. Then the node module directory was deleted

--- NPM-Demo/index.js
+++ NPM-Demo/index.js
@@ -1,5 +1,10 @@
 import os from 'os';//Core module 
 import sum from './calculator.js'; //Internal module
+import calculator from 'calculator'; //Vendor module
 
 console.log(`Hello from NodeJS ${os.EOL} new line`); 
 console.log(sum(2,5));
+
+const f = calculator.func('f(x) = x*10 - 20');
+console.log(f(3));
+
