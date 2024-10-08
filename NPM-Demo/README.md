2df113613d6992e4f6cfa07ef01582ff9c681d1e
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-10-08 12:00:19 +0300

    modules examples

--- NPM-Demo/README.md
+++ NPM-Demo/README.md
@@ -1,2 +1,8 @@
-## NPM & Modules
+## NPM 
 1. `npm init -y`
+2. Define start script and run with `npm start`
+
+## Modules
+1. Core - nodejs modules
+2. Internal - custom
+3. External - vendor 
--- NPM-Demo/index.js
+++ NPM-Demo/index.js
@@ -1,1 +1,7 @@
-console.log("Hello from NodeJS");
+import {log} from 'console';
+import os from 'os';
+
+console.log(`Hello from NodeJS ${os.EOL} new line`);
+
+log(os.EOL.charCodeAt(0));
+log(os.EOL.charCodeAt(1));
