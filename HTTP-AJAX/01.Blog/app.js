1724c27c8a96c35ce7ff7cb468ed36ffa639b249
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-10-16 01:00:18 +0300

    nearly done exercise

--- HTTP-AJAX/01.Blog/app.js
+++ HTTP-AJAX/01.Blog/app.js
@@ -7,14 +7,17 @@ function attachEvents() {
     const selectPostElement = document.getElementById("posts");
     const viewPostButtonElement = document.getElementById("btnViewPost");
     const postBodyElement = document.getElementById("post-body");
+    const postTitleElement = document.getElementById("post-title");
     const postCommentsElement = document.getElementById("post-comments");
 
-    async function customFetch(url){ 
+    async function customFetch(url) {
         return fetch(url)
             .then((response) => response.json());
     }
 
     function appendPosts(postResponse) {
+        selectPostElement.textContent = "";
+
         Object.values(postResponse).forEach(({ body, id, title }) => {
             const optionElement = document.createElement("option");
 
@@ -37,7 +40,29 @@ function attachEvents() {
             return;
         }
 
-     }
+        customFetch(`${postsUrl}/${selectedPostId}`)
+            .then(({ body, title }) => {
+                postBodyElement.textContent = body;
+                postTitleElement.textContent = title;
+
+                return customFetch(commentsUrl);
+            })
+            .then((commentsReponse) => {
+                postCommentsElement.textContent = "";
+
+                Object.values(commentsReponse)
+                    .filter(({ postId }) => postId === selectedPostId)
+                    .forEach(({ id, text }) => {
+                        const liElement = document.createElement("li");
+
+                        liElement.textContent = text;
+                        liElement.setAttribute('data-id', id);
+
+                        postCommentsElement.appendChild(liElement);
+                    });
+            });
+    }
+    
     loadButtonElement.addEventListener("click", fetchAllPosts);
     viewPostButtonElement.addEventListener("click", fetchSinglePost);
 
