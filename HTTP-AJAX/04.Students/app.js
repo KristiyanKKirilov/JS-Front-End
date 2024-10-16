6e0193406f909b105ec14fe3e8bdccbb0996f260
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-10-16 23:36:37 +0300

    4th exercise done

--- HTTP-AJAX/04.Students/app.js
+++ HTTP-AJAX/04.Students/app.js
@@ -4,69 +4,87 @@ function attachEvents() {
   const [firstNameInputEl, lastNameInputEl, facultyNumberInputEl, gradeInputEl] = document.querySelectorAll("input[type=text]");
   const submitBtnEl = document.getElementById("submit");
 
-  async function customFetch(url, options){
-    return fetch(url, options).then(response => response.json());
-  }
+  async function customFetch(url, options) {
+    return fetch(url, options)
+      .then(response => {
 
-  function loadAllStudentsHandler(){
-    customFetch(baseUrl)
-      .then(result => {
-        Object.values(result).forEach(({firstName, lastName, facultyNumber, grade}) => {
-          const trEl = document.createElement("tr");
+        if (!response.ok) {
+          throw Error(response.message);
+        }
+
+        return response.json()
+      });
+  }
+    
+  function appendStudents({ firstName, lastName, facultyNumber, grade }){
+    const trEl = document.createElement("tr");
 
           const tdFirstNameEl = document.createElement("td");
           tdFirstNameEl.textContent = firstName;
-          
+
           const tdLastNameEl = document.createElement("td");
           tdLastNameEl.textContent = lastName;
-          
+
           const tdFacultyNumberEl = document.createElement("td");
           tdFacultyNumberEl.textContent = facultyNumber;
-          
+
           const tdGradeEl = document.createElement("td");
           tdGradeEl.textContent = grade;
 
           trEl.append(tdFirstNameEl, tdLastNameEl, tdFacultyNumberEl, tdGradeEl);
           tbodyEl.appendChild(trEl);
-        })
-      });
+  }
+
+  function loadAllStudentsHandler() {
+    try {
+      customFetch(baseUrl)
+      .then(result => {
+        Object.values(result).forEach(appendStudents);
+      }).catch(err => console.log(err));
+    } catch (error) {
+      console.log(error.message);
     }
+    
+  }
+  function clearInputs(){
+    firstNameInputEl.value = "";
+    lastNameInputEl.value = "";
+    facultyNumberInputEl.value = "";
+    gradeInputEl.value = "";
+  }
 
-    function createStudentHandler(){
-      if(firstNameInputEl.value && lastNameInputEl.value && facultyNumberInputEl.value && gradeInputEl.value){
-        customFetch(baseUrl, {
-          method: "POST",
-          headers: {
-            "Content-Type" : "application/json",
-          },
-          body: JSON.stringify({
-            firstName: firstNameInputEl.value,
-            lastName: lastNameInputEl.value,
-            facultyNumber: facultyNumberInputEl.value,
-            grade: gradeInputEl.value
-          })
-        }).then(() => {
-          tbodyEl.innerHTML = "";
-          
-          firstNameInputEl.value = "";
-          lastNameInputEl.value = "";
-          facultyNumberInputEl.value = "";
-          gradeInputEl.value = "";          
-
-          loadAllStudentsHandler();
+  function createStudentHandler() {
+    if (firstNameInputEl.value && lastNameInputEl.value && facultyNumberInputEl.value && gradeInputEl.value) {
+      customFetch(baseUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          firstName: firstNameInputEl.value,
+          lastName: lastNameInputEl.value,
+          facultyNumber: facultyNumberInputEl.value,
+          grade: gradeInputEl.value
         })
+      }).then(() => {
+        tbodyEl.innerHTML = "";  
+
+        clearInputs();
+        loadAllStudentsHandler();
+      })
         .catch(err => console.log(err));
-      }
-      else{
-        return;
-      }
-     
+    }
+    else {
+      clearInputs();
+      return;
     }
 
-    loadAllStudentsHandler();
-    submitBtnEl.addEventListener("click", createStudentHandler);
+  }
 
-} 
+  loadAllStudentsHandler();
+  submitBtnEl.addEventListener("click", createStudentHandler);
+
+}
 
 
 attachEvents();
