19f38be997b0ece9a4cfc341b809b7065a731ceb
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-10-16 15:56:10 +0300

    2nd exercise done. Fixed slow proceesing issue

--- HTTP-AJAX/02.Messenger/app.js
+++ HTTP-AJAX/02.Messenger/app.js
@@ -1,7 +1,6 @@
 function attachEvents() {
     const baseUrl = "http://localhost:3030/jsonstore/messenger";
     const textAreaElement = document.getElementById("messages");
-    console.log(textAreaElement);
      const [authorInputElement, messageInputElement] = document.querySelectorAll("#controls div input[type=text]");
     const submitBtnElement = document.getElementById("submit");
     const refreshBtnElement = document.getElementById("refresh");
@@ -19,21 +18,16 @@ function attachEvents() {
                 author: authorInputElement.value,
                 content: messageInputElement.value
             })
+        }).then(() => {
+            authorInputElement.value = "";
+            messageInputElement.value = "";
         });
     }
 
     function appendMessages(messagesResult){
-        textAreaElement.value = "";
-        Object.values(messagesResult).forEach(({author, content}) => {
-            console.log(author);
-            const pEl = document.createElement("p");
-            pEl.textContent = author ? `${author}: ${content}\n`: '';
-            console.log(pEl.textContent);
-            textAreaElement.value += pEl.textContent ? pEl.textContent : '';
-        })
-
-        textAreaElement.value = textAreaElement.value.trimEnd();
-    }
+        textAreaElement.textContent = Object.values(messagesResult)
+            .map(({author, content}) =>`${author}: ${content}`).join("\n");
+        }
 
     function getAllMessagesHandler(){
         customFetch(baseUrl)
