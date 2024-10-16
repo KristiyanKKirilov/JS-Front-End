07066a0e6cae41d781a810488cad5183de7eab75
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-10-16 15:36:57 +0300

    2nd exercise using promises done

--- HTTP-AJAX/02.Messenger/app.js
+++ HTTP-AJAX/02.Messenger/app.js
@@ -1,5 +1,47 @@
 function attachEvents() {
-    // TODO:
+    const baseUrl = "http://localhost:3030/jsonstore/messenger";
+    const textAreaElement = document.getElementById("messages");
+    console.log(textAreaElement);
+     const [authorInputElement, messageInputElement] = document.querySelectorAll("#controls div input[type=text]");
+    const submitBtnElement = document.getElementById("submit");
+    const refreshBtnElement = document.getElementById("refresh");
+ 
+    async function customFetch(url, options){
+        return fetch(url, options).then(response => response.json());
+    }
+    function createMessageHandler(){
+        customFetch(baseUrl, {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                author: authorInputElement.value,
+                content: messageInputElement.value
+            })
+        });
+    }
+
+    function appendMessages(messagesResult){
+        textAreaElement.value = "";
+        Object.values(messagesResult).forEach(({author, content}) => {
+            console.log(author);
+            const pEl = document.createElement("p");
+            pEl.textContent = author ? `${author}: ${content}\n`: '';
+            console.log(pEl.textContent);
+            textAreaElement.value += pEl.textContent ? pEl.textContent : '';
+        })
+
+        textAreaElement.value = textAreaElement.value.trimEnd();
+    }
+
+    function getAllMessagesHandler(){
+        customFetch(baseUrl)
+            .then(result => appendMessages(result));
+    }
+
+    submitBtnElement.addEventListener("click", createMessageHandler);   
+    refreshBtnElement.addEventListener("click", getAllMessagesHandler);
 }
 
 attachEvents();
