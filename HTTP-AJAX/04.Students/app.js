ef713a35cdd6df1a42c4d18414c9ee6795c01160
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-10-16 22:56:12 +0300

    load all students data

--- HTTP-AJAX/04.Students/app.js
+++ HTTP-AJAX/04.Students/app.js
@@ -1,5 +1,40 @@
 function attachEvents() {
-  // TODO:
-}
+  const baseUrl = "http://localhost:3030/jsonstore/collections/students";
+  const tbodyEl = document.querySelector("#results tbody");
+  const [firstNameInputEl, lastNameInputEl, facultyNumberEl, gradeEl] = document.querySelectorAll("input[type=text]");
+  const submitBtnEl = document.getElementById("submit");
+
+  async function customFetch(url){
+    return fetch(url).then(response => response.json());
+  }
+
+  function loadAllStudentsHandler(){
+    customFetch(baseUrl)
+      .then(result => {
+        Object.values(result).forEach(({firstName, lastName, facultyNumber, grade}) => {
+          const trEl = document.createElement("tr");
+
+          const tdFirstNameEl = document.createElement("td");
+          tdFirstNameEl.textContent = firstName;
+          
+          const tdLastNameEl = document.createElement("td");
+          tdLastNameEl.textContent = lastName;
+          
+          const tdFacultyNumberEl = document.createElement("td");
+          tdFacultyNumberEl.textContent = facultyNumber;
+          
+          const tdGradeEl = document.createElement("td");
+          tdGradeEl.textContent = grade;
+
+          trEl.append(tdFirstNameEl, tdLastNameEl, tdFacultyNumberEl, tdGradeEl);
+          tbodyEl.appendChild(trEl);
+        })
+      });
+    }
+
+    loadAllStudentsHandler();
+
+} 
+
 
 attachEvents();
