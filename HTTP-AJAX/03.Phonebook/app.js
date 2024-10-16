ae159450a2b28c378ae59c8940cae92e9df60836
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-10-16 18:37:04 +0300

    loading phones done; get request using promise  async function

--- HTTP-AJAX/03.Phonebook/app.js
+++ HTTP-AJAX/03.Phonebook/app.js
@@ -1,5 +1,36 @@
 function attachEvents() {
-    // TODO:
+    const baseUrl = "http://localhost:3030/jsonstore/phonebook";
+
+    const ulPhoneBookElement = document.getElementById("phonebook");
+    const loadBtnElement = document.getElementById("btnLoad");
+    const [personInputElement, phoneInputElement] = document.querySelectorAll("input[type=text]");
+
+    async function customFetch(url, options){
+        return fetch(url, options)
+            .then(response => response.json());
+    }
+
+    function deletePhoneHandler(){
+
+    }
+    function getAllPhonesHandler(){
+        customFetch(baseUrl)
+            .then(result => {
+                Object.values(result).forEach(({person, phone, id}) => {
+                    const liElement = document.createElement("li");
+                    liElement.textContent = `${person}: ${phone}`;
+
+                    const deleteBtnElement = document.createElement("button");
+                    deleteBtnElement.textContent = "Delete";
+                    deleteBtnElement.addEventListener("click", deletePhoneHandler);
+               
+                    liElement.appendChild(deleteBtnElement);
+                    ulPhoneBookElement.appendChild(liElement);
+                })
+            })
+    }
+
+    loadBtnElement.addEventListener("click", getAllPhonesHandler);
 }
 
 attachEvents();
